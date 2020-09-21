a2dbb7f783657092b20c88c0c666550a0258e2d8
Cristian Piedrahita <cristian.piedrahita.1209@eam.edu.co>
2020-09-21 00:27:31 -0500

    ser

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -1,33 +1,29 @@
 pipeline {
-
     environment {
-        BASE_GIT_URL = 'https://github.com/cristianpt21'
-        APP_REPO_URL = "${env.BASE_GIT_URL}/${repo}.git"
+        BASE_REPO_URL = 'https://github.com/cristianpt21'
+        APP_REPO_URL = "${env.BASE_REPO_URL}/${repo}.git"
         INFRA_REPO_URL = "${env.BASE_GIT_URL}/infraestructura.git"
         DOCKER_IMAGE = "crisapt1/${repo}"
         DEPLOY_FOLDER = "deploy/kubernete/${repo}"
     }
-
     agent any
     stages {
         stage("Checkout app-code") {
             steps {
             //se esta crando una carpeta....
-               dir('app') {
+                dir('app') {
                     git url:"${env.APP_REPO_URL}" , branch: "${version}"
                 } 
             }
         }
-         
-         stage("Checkout deploy-code") {
+        stage("Checkout deploy-code") {
             steps {
-               dir('deploy') {
+                dir('deploy') {
                     git url:"${env.INFRA_REPO_URL}" , branch: "master"
                 } 
             }
         }
-        
-         stage("Build image") {
+        stage("Build image") {
             steps {
                 dir('app') {
                     script {
@@ -36,29 +32,27 @@ pipeline {
                 }
             }
         }
-        
         stage("Push image") {
             steps {
                 script {
                     docker.withRegistry('', 'cristiandocker') {
-                    dockerImage.push()
+                        dockerImage.push()
                     }
                 }
             }
         }
-        
         stage('Deploy') {
             steps{
                 sh "sed -i 's:DOCKER_IMAGE:${env.DOCKER_IMAGE}:g' ${DEPLOY_FOLDER}/deployment.yaml"
                 sh "sed -i 's:TAG:${tag}:g' ${DEPLOY_FOLDER}/deployment.yaml"
                 
-                step([$class: 'KubernetesEngineBuilder', 
-                        projectId: "airy-actor-288822",
-                        clusterName: "cluster-test",
-                        zone: "us-central1-a",
-                        manifestPattern: "${DEPLOY_FOLDER}/",
-                        credentialsId: "infraestructura",
-                        verifyDeployments: true])
+                step([$class: 'KubernetesEngineBuilder',
+                projectId: "airy-actor-288822",
+                clusterName: "cluster-test",
+                zone: "us-central1-a",
+                manifestPattern: "${DEPLOY_FOLDER}/",
+                credentialsId: "infraestructura",
+                verifyDeployments: true])
             }
         }
     }
